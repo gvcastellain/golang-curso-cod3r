4bc9ed48f1595c4bd5232b90883eec16115dd1b2
gvcastellain <gabriel.castellain@gmail.com>
2024-02-12 11:08:41 -0300

    Executnado um select parametrizado no bd e passando o retorno da consulta para uma struct

---
+++ curso_cod3r/sql/select/select.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type usuario struct {
+	id   int
+	nome string
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:123456@/cursogo")
+
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	stmt, _ := db.Query("select id, nome from usuarios where id > ?", 0)
+	defer stmt.Close()
+
+	for stmt.Next() {
+		var user usuario
+		stmt.Scan(&user.id, &user.nome)
+		fmt.Println(user)
+	}
+}
